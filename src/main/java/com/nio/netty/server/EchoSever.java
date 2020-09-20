fba1002145272570939cd814cfc393ad2af4b07c
DJ <769979801@qq.com>
2020-09-20 21:49:23 +0800

    init2

--- src/main/java/com/nio/netty/server/EchoSever.java
+++ src/main/java/com/nio/netty/server/EchoSever.java
@@ -2,10 +2,8 @@ package com.nio.netty.server;
 
 import com.nio.netty.server.handler.EchoServerHandler;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -14,7 +12,7 @@ public class EchoSever {
     public static void main(String[] args) throws InterruptedException {
         ServerBootstrap bootstrap = new ServerBootstrap();
         bootstrap
-                .group(new NioEventLoopGroup())
+                .group(new NioEventLoopGroup(1),new NioEventLoopGroup())
                 .channel(NioServerSocketChannel.class)
                 .localAddress(9988)
                 .childHandler(new ChannelInitializer<SocketChannel>() {
